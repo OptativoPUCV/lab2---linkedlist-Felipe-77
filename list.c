964799d40667ea1b64f59605b2d601a97a5815d3
Your Name <you@example.com>
2023-03-20 17:09:21 -0300

    new try

--- list.c
+++ list.c
@@ -47,14 +47,9 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
-    if (list->current == NULL) //verificamos que el current no sea nulo
-        return NULL;
-    else if (list->current->next == NULL) { //verificamos que el current no sea el ultimo dato de la lista
-        return NULL;
-    }
-
     list->current = list->current->next;
-    return list->current->data;
+
+    return (list->current == NULL) ? NULL : list->current->data;
 }
 
 void * lastList(List * list) {
